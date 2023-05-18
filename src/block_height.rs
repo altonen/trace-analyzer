62dcb9ba1989c6961ca85fbe1516cabcd4a20fb3
Aaro Altonen <a.altonen@hotmail.com>
2023-05-18 20:27:44 +0300

    block-height: Collect best and finalized block heights

--- src/block_height.rs
+++ src/block_height.rs
@@ -3,10 +3,18 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
+struct BlockHeight {
+    time: String,
+    best: usize,
+    finalized: usize,
+}
+
 pub fn analyze_block_height(reader: BufReader<File>) -> Result<(), Box<dyn Error>> {
     // let re = Regex::new(r"target=#(\d+).*best: #(\d+).*finalized #(\d+)").unwrap();
     // let re = Regex::new(r"r'(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}.\d{3}).*target=#(\d+).*best: #(\d+).*finalized #(\d+)'").unwrap();
     let re = Regex::new(r"(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}.\d{3}).*target=#(\d+).*best: #(\d+).*finalized #(\d+)").unwrap();
+
+    let mut block_heights = Vec::new();
 
     for line in reader.lines() {
         let line = line?;
@@ -16,18 +24,20 @@ pub fn analyze_block_height(reader: BufReader<File>) -> Result<(), Box<dyn Error
                 continue;
             }
             Some(captures) => {
-                // if captures.len() != 4 {
-                //     continue;
-                // }
-
-                println!("{:?}", &captures[2]);
-
-                // println!(
-                //     "target {}, best {}, finalize {}",
-                //     &captures[1], &captures[2], &captures[3]
-                // );
+                block_heights.push(BlockHeight {
+                    time: captures[2].to_string(),
+                    best: captures[4].parse::<usize>().unwrap(),
+                    finalized: captures[5].parse::<usize>().unwrap(),
+                });
             }
         }
+    }
+
+    for block_height in block_heights {
+        println!(
+            "{}: best {} finalized {}",
+            block_height.time, block_height.best, block_height.finalized
+        );
     }
 
     Ok(())
